081b5896e0f4af3eac23413e800c1f619243463d
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-23 18:45:32 +0200

    compaiono con batman

--- main.js
+++ main.js
@@ -1,28 +1,72 @@
 $(document).ready(function() {
+
     //input testuale e pulsante
     // quando l'utente clicca il pulsante leggo il testo
     // con quel testo lì vado a compilare la mia query
     // devo stampare titolo titolo originale lingua e voto
     //BONUS: scelta risultati lingua
 
-    var ricerca = 'batman'
+    //dichiaro le variabili che mi serviranno con Handlebars
+    var source = $('#entry-template').html();
+    var template = Handlebars.compile(source);
+    // intercetto il click sul pulsante di ricerca
+    $('#pulsante-ricerca').click(function(){
 
-$.ajax({
-    'url': 'https://api.themoviedb.org/3/search/movie',//questo non basta per avere una risposta dall'api. ha bisogno anche della chiave api key e della query (il valore della ricerca), altrimenti non mi restituisce niente
-    'method': 'GET',
-    'data': {
-        'api_key': '702eb04790fdfb1eb76d04db48beddc9',
-        'query': ricerca
-        //'language': ''
-    }//questo data è un oggetto che non ha nulla a che vedere con la funzione (data)
-    'success': function (data) {
-        console.log(data);
-    },
-    'error': function () {
-        console.log('errore');
-    }
+        $.ajax({
+            'url': 'https://api.themoviedb.org/3/search/movie',//questo non basta per avere una risposta dall'api. ha bisogno anche della chiave api key e della query (il valore della ricerca), altrimenti non mi restituisce niente
+            'method': 'GET',
+            'data': {
+                'api_key': '702eb04790fdfb1eb76d04db48beddc9',
+                //con il testo letto vado a compilare la mia query
+                'query': 'batman'
+                //'language': ''
+            },//questo data è un oggetto che non ha nulla a che vedere con la funzione (data)
+            'success': function (data) {
+                var film = data.results;
+                console.log('array dei film di batman:');
+                console.log(film);
+                //ciclo l'array per leggere tutti gli oggetti all'interno
+                for (var i = 0; i < film.length; i++) {
+                    var film_corrente = film[i];
+
+                    //mi tiro fuori tutte le proprietà che mi servono:
+                    var titolo_originale = film_corrente.original_title;
+                    var lingua = film_corrente.original_language;
+                    var voto = film_corrente.vote_average;
+                    console.log('film:');
+
+                    // imposto le proprietà dell'oggetto context e le compilo con le proprietà recuperate da ogni film corrente
+                    var context = {
+                        "titolo-originale" : titolo_originale,
+                        "lingua" : lingua,
+                        "voto" : voto
+                    };//fine context
+                    console.log('context');
+                    console.log(context);
+
+                    //compilo il template con le proprietà inserite dentro context
+                    var scheda_film = template(context);
+                    console.log(scheda_film);
+
+                    //...e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+                    $('.container').append(scheda_film);
+
+                }//fine ciclo for
 
 
-}); //fine ajax
+
+            },
+            'error': function () {
+                console.log('errore');
+            }
+
+
+        }); //fine ajax
+
+    });//fine funzione click
+
+
+
+
 
 });//fine document ready
