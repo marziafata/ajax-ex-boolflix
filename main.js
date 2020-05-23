b5e606955396164e399b175ccb2211cb098ce787
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-23 19:20:26 +0200

    ok ricerca ok handlebars

--- main.js
+++ main.js
@@ -12,37 +12,41 @@ $(document).ready(function() {
     // intercetto il click sul pulsante di ricerca
     $('#pulsante-ricerca').click(function(){
 
+        //leggo il testo nell'input
+        var ricerca = $('#testo-ricerca').val();
+
         $.ajax({
             'url': 'https://api.themoviedb.org/3/search/movie',//questo non basta per avere una risposta dall'api. ha bisogno anche della chiave api key e della query (il valore della ricerca), altrimenti non mi restituisce niente
             'method': 'GET',
             'data': {
                 'api_key': '702eb04790fdfb1eb76d04db48beddc9',
                 //con il testo letto vado a compilare la mia query
-                'query': 'batman'
-                //'language': ''
+                'query': ricerca,
+                'language': 'it'
             },//questo data è un oggetto che non ha nulla a che vedere con la funzione (data)
             'success': function (data) {
                 var film = data.results;
-                console.log('array dei film di batman:');
                 console.log(film);
+
                 //ciclo l'array per leggere tutti gli oggetti all'interno
                 for (var i = 0; i < film.length; i++) {
                     var film_corrente = film[i];
 
                     //mi tiro fuori tutte le proprietà che mi servono:
+                    var titolo = film_corrente.title;
                     var titolo_originale = film_corrente.original_title;
                     var lingua = film_corrente.original_language;
                     var voto = film_corrente.vote_average;
-                    console.log('film:');
+
 
                     // imposto le proprietà dell'oggetto context e le compilo con le proprietà recuperate da ogni film corrente
                     var context = {
+                        "titolo" : titolo,
                         "titolo-originale" : titolo_originale,
                         "lingua" : lingua,
                         "voto" : voto
                     };//fine context
-                    console.log('context');
-                    console.log(context);
+
 
                     //compilo il template con le proprietà inserite dentro context
                     var scheda_film = template(context);
@@ -52,8 +56,6 @@ $(document).ready(function() {
                     $('.container').append(scheda_film);
 
                 }//fine ciclo for
-
-
 
             },
             'error': function () {
