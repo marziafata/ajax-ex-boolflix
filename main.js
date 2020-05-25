ea5c519fcd8a66ce19d5365b19e955efbe55387f
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-25 16:23:41 +0200

    controllo sul testo digitato dall'utente (almeno 2 caratteri)

--- main.js
+++ main.js
@@ -10,84 +10,90 @@ $(document).ready(function() {
         //leggo il testo nell'input
         var ricerca = $('#testo-ricerca').val().trim();
 
-        //inserisco il testo cercato dall'utente nel titolo della pagina
-        $('span.ricerca-utente').text(ricerca);
+        //controllo che l'utente abbia digitato qualcosa
+        if (ricerca.length >= 2) { //oppure potevo mettere ricerca != diverso da '' stringa vuota
 
-        // resetto l'input
-        $('#testo-ricerca').val('');
+                    //inserisco il testo cercato dall'utente nel titolo della pagina
+                    $('span.ricerca-utente').text(ricerca);
 
-        //svuoto il container dai risultati precedenti
-        // $('.scheda-film').remove(); //rischioso se ho stessa classe da altre parti. meglio mettere un selettore davanti
-        // $('.container').html(''); // setta l'html del container a svuoto
-        $('.ricerca').empty(''); //svuota il container
-        // il reset dell'input e del container meglio farli sempre vicini
+                    // resetto l'input
+                    $('#testo-ricerca').val('');
 
-        //nascondo il titolo
-        $('.titolo-ricerca').removeClass('visible');
+                    //svuoto il container dai risultati precedenti
+                    // $('.scheda-film').remove(); //rischioso se ho stessa classe da altre parti. meglio mettere un selettore davanti
+                    // $('.container').html(''); // setta l'html del container a svuoto
+                    $('.ricerca').empty(''); //svuota il container
+                    // il reset dell'input e del container meglio farli sempre vicini
 
-        $.ajax({
-            'url': 'https://api.themoviedb.org/3/search/movie',//questo non basta per avere una risposta dall'api. ha bisogno anche della chiave api key e della query (il valore della ricerca), altrimenti non mi restituisce niente
-            'method': 'GET',
-            'data': {
-                'api_key': '702eb04790fdfb1eb76d04db48beddc9',
-                //con il testo letto vado a compilare la mia query
-                'query': ricerca,
-                'language': 'it'
-            },//questo data è un oggetto che non ha nulla a che vedere con la funzione (data)
-            'success': function (data) {
+                    //nascondo il titolo
+                    $('.titolo-ricerca').removeClass('visible');
 
-                //aggiungo la classe visible al titolo per visualizzarlo
-                $('.titolo-ricerca').addClass('visible');
-                var film = data.results;
-                console.log(film);
+                    //faccio partire la chiamata ajax
+                    $.ajax({
+                        'url': 'https://api.themoviedb.org/3/search/movie',//questo non basta per avere una risposta dall'api. ha bisogno anche della chiave api key e della query (il valore della ricerca), altrimenti non mi restituisce niente
+                        'method': 'GET',
+                        'data': {
+                            'api_key': '702eb04790fdfb1eb76d04db48beddc9',
+                            //con il testo letto vado a compilare la mia query
+                            'query': ricerca,
+                            'language': 'it'
+                        },//questo data è un oggetto che non ha nulla a che vedere con la funzione (data)
+                        'success': function (data) {
 
-                //ciclo l'array per leggere tutti gli oggetti all'interno
-                for (var i = 0; i < film.length; i++) {
-                    var film_corrente = film[i];
+                            //aggiungo la classe visible al titolo per visualizzarlo
+                            $('.titolo-ricerca').addClass('visible');
+                            var film = data.results;
+                            console.log(film);
 
-                    //mi tiro fuori tutte le proprietà che mi servono:
-                    var titolo = film_corrente.title;
-                    var titolo_originale = film_corrente.original_title;
-                    var lingua = film_corrente.original_language;
-                    var voto = film_corrente.vote_average;
+                            //ciclo l'array per leggere tutti gli oggetti all'interno
+                            for (var i = 0; i < film.length; i++) {
+                                var film_corrente = film[i];
 
-
-                    // ESEMPIO PER APPENDERE HTML senza HANDLEBARS ma poco elegante, quindi sconsigliato
-                    // var dati_film = '<ul>';
-                    // dati_film += '<li>' + titolo + '</li>';//+= serve a creare e concatenare contemporaneamente una variabile: prende quello che c'era già salvato nella variabile e ci aggiunge il nuovo valore.
-                    // dati_film += '<li>' + titolo_originale + '</li>';
-                    // dati_film += '<li>' + lingua + '</li>';
-                    // dati_film += '<li>' + voto + '</li>';
-                    // dati_film = '</ul>';
-                    // $('.container').append(dati_film);
+                                //mi tiro fuori tutte le proprietà che mi servono:
+                                var titolo = film_corrente.title;
+                                var titolo_originale = film_corrente.original_title;
+                                var lingua = film_corrente.original_language;
+                                var voto = film_corrente.vote_average;
 
 
-
-                    // imposto le proprietà dell'oggetto context e le compilo con le proprietà recuperate da ogni film corrente
-                    var context = {
-                        "titolo" : titolo,
-                        "titolo-originale" : titolo_originale,
-                        "lingua" : lingua,
-                        "voto" : voto
-                    };//fine context
-
-
-                    //compilo il template con le proprietà inserite dentro context
-                    var scheda_film = template(context);
-                    console.log(scheda_film);
-
-                    //...e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
-                    $('.ricerca').append(scheda_film);
-
-                }//fine ciclo for
-
-            },// fine success
-            'error': function () {
-                console.log('errore');
-            }// fine error
+                                // ESEMPIO PER APPENDERE HTML senza HANDLEBARS ma poco elegante, quindi sconsigliato
+                                // var dati_film = '<ul>';
+                                // dati_film += '<li>' + titolo + '</li>';//+= serve a creare e concatenare contemporaneamente una variabile: prende quello che c'era già salvato nella variabile e ci aggiunge il nuovo valore.
+                                // dati_film += '<li>' + titolo_originale + '</li>';
+                                // dati_film += '<li>' + lingua + '</li>';
+                                // dati_film += '<li>' + voto + '</li>';
+                                // dati_film = '</ul>';
+                                // $('.container').append(dati_film);
 
 
-        }); //fine ajax
+
+                                // imposto le proprietà dell'oggetto context e le compilo con le proprietà recuperate da ogni film corrente
+                                var context = {
+                                    "titolo" : titolo,
+                                    "titolo-originale" : titolo_originale,
+                                    "lingua" : lingua,
+                                    "voto" : voto
+                                };//fine context
+
+                                //compilo il template con le proprietà inserite dentro context
+                                var scheda_film = template(context);
+                                console.log(scheda_film);
+
+                                //...e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+                                $('.ricerca').append(scheda_film);
+
+                            }//fine ciclo for
+
+                        },// fine success
+                        'error': function () {
+                            console.log('errore');
+                        }// fine error
+
+                    }); //fine ajax
+        } else {
+            alert('Digita qualcosa!');
+        }
+
 
     });//fine funzione click
 
